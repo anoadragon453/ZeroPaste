3ddd546b0eb06355adf0020fea7215436e082bca
Andrew Morgan (https://amorgan.xyz) <andrew@amorgan.xyz>
2017-12-09 19:20:54 -0800

    Grab pasteID 'p' from URL on load
    Reorganize files a little bit

--- js/index.js
+++ js/index.js
@@ -1,5 +1,13 @@
-window.zeroFrame = new ZeroFrame();
-window.zeroPage = new ZeroPage(zeroFrame);
-window.zeroFS = new ZeroFS(zeroPage);
+// Gets a parameter from the URL
+// i.e. p <- http://127.0.0.1:43110/site.bit?p=hello
+function getURLParameter(name) {
+  return decodeURIComponent((new RegExp('[?|&]' + name + '=' + '([^&;]+?)(&|#|;|$)').exec(location.search) || [null, ''])[1].replace(/\+/g, '%20')) || null;
+}
 
-
+// Check if the user supplied a paste
+// Paste IDs can be supplied in the URL with the 'p' param
+var pasteID = getURLParameter('p');
+if(pasteID) {
+    console.log("Loading paste ID: " + pasteID);
+    loadPaste(pasteID);
+}
--- js/paste.js
+++ js/paste.js
@@ -1,12 +1,19 @@
-createNewPaste() {
+// Create a new paste and insert its contents into the user's
+// data.json
+function createNewPaste() {
     // Get the paste content
     pasteContent = document.getElementById("paste-area").value;
 
     // Check if encryption is enabled
-    //
+
     // Encrypt the paste
 
     // Get the user's data JSON
 
     // Insert the paste
+}
+
+// Load the supplied pasteID from the DB (if it exists)
+function loadPaste(id) {
+
 }
