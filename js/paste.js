072be4de8e57a3a93ed5d4fec1e046dff993d800
Andrew Morgan (https://amorgan.xyz) <andrew@amorgan.xyz>
2017-12-09 16:12:41 -0800

    Add encrypted checkbox and change save button text

---
+++ js/paste.js
@@ -0,0 +1,12 @@
+createNewPaste() {
+    // Get the paste content
+    pasteContent = document.getElementById("paste-area").value;
+
+    // Check if encryption is enabled
+    //
+    // Encrypt the paste
+
+    // Get the user's data JSON
+
+    // Insert the paste
+}
